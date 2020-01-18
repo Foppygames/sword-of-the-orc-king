a6a40aa5c1dbc5e1e8e5be5a76440341a8faa79c
Robbert Prins <robbertprins@hotmail.com>
2020-01-18 01:36:52 +0100

    Added camera component to repository

---
+++ modules/ecs/components/camera.lua
@@ -0,0 +1,14 @@
+local Component = require("modules.ecs.components.component")
+
+local Camera = {}
+
+function Camera.create(entityDefaults,entityData)
+    local self = Component.create({
+    })
+
+    self.setValues(entityDefaults,entityData)
+
+    return self
+end
+
+return Camera
